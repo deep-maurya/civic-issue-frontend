ed7e94e8280420bc550b91884701aac534eb07e3
deep-maurya <deepakmauryahd@gmail.com>
2025-10-26 03:51:11 +0530

    Logo removed

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -25,12 +25,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       </div>
       <div className="flex flex-col gap-4 p-6 md:p-10 lg:col-span-2">
         <div className="flex justify-center items-center gap-2">
-          <a href="#" className="flex items-center gap-2 font-medium">
-            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-              <GalleryVerticalEnd className="size-4" />
-            </div>
-            Acme Inc.
-          </a>
+
         </div>
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-xs">{children}</div>
