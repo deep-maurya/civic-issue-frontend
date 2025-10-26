b63d5065b29c3194569d83086d8031163408d89a
deep-maurya <deepakmauryahd@gmail.com>
2025-10-26 16:38:04 +0530

    Platform name updated

--- src/components/PublicPages/Footer.tsx
+++ src/components/PublicPages/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
       {/* Footer */}
       <footer className=" border-t border-border bg-background">
       <div className="p-5 border-t border-border text-center text-sm text-foreground/70">
-            <p>&copy; 2025 OneQR. All rights reserved. Made with ❤️ for everyone.</p>
+            <p>&copy; 2025 Public Pulse. All rights reserved. Made with ❤️ for everyone.</p>
           </div>
       </footer>
     </>
